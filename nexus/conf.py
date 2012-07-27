3e870b1c84ffc4fddca9104431c406dccc8a1ada
Artem melanich <pkeeper.shark@mail.ru>
2012-07-27 21:05:50 +0300

    update manifest and defauls media url

--- nexus/conf.py
+++ nexus/conf.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-MEDIA_PREFIX = getattr(settings, 'NEXUS_MEDIA_PREFIX', '/nexus/media/')
+MEDIA_PREFIX = getattr(settings, 'NEXUS_MEDIA_PREFIX', '/static/nexus/')
 
 if getattr(settings, 'NEXUS_USE_DJANGO_MEDIA_URL', False):
     MEDIA_PREFIX = getattr(settings, 'MEDIA_URL', MEDIA_PREFIX)
